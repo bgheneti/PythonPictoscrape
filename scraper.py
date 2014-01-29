7391724d540a9e151510528fed9d54180f04cdf2
bgheneti <bgheneti@mit.edu>
2014-01-29 15:20:56 -0500

    ADULT

--- scraper.py
+++ scraper.py
@@ -5,35 +5,49 @@ from rfc3987 import parse
 
 def webCheck(website):
 
-	try:
-		print parse(website, rule='IRI')
-		return True	
-	except:
-		return False
+        try:
+                print parse(website, rule='IRI')
+                return True        
+        except:
+                return False
 #checks if website URL exists
 
 def urlRead(website):
-	page = requests.get(website)
-	tree=html.fromstring(page.text)
-	subject = tree.xpath('//dd[@class="fandom tags"]/ul/*[1]/a[@class="tag"]/text()')
-	summary = tree.xpath('//blockquote[@class="userstuff"]/p/text()')
-	storyOrig = tree.xpath('//div[@class="userstuff"]/p/text()')
-	story=[]
-	for i in xrange(len(storyOrig)):
-		part=storyOrig[i].strip().encode('ascii', 'ignore')
-		if part!='' and part!='-':
-			story.append(part)
-	story = " ".join(story);
-	return [subject,summary,story]
+        page = requests.get(website)
+        tree=html.fromstring(page.text)
+        print tree
+        subject = tree.xpath('//dd[@class="fandom tags"]/ul/*[1]/a[@class="tag"]/text()')
+        if subject==[]:
+        	page=requests.get(website+"?view_adult=true")
+        	tree=html.fromstring(page.text)
+        	subject = tree.xpath('//dd[@class="fandom tags"]/ul/*[1]/a[@class="tag"]/text()')
+
+        print subject
+        summaryOrig = tree.xpath('//blockquote[@class="userstuff"]/p/text()')
+        storyOrig = tree.xpath('//div[@role="article"]/p/text()')
+        story=[]
+        summary=[]
+        for i in xrange(len(storyOrig)):
+                part=storyOrig[i].strip().encode('ascii', 'ignore')
+                if part!='' and part!='-':
+                        story.append(part)
+        for i in xrange(len(summaryOrig)):
+                part=summaryOrig[i].strip().encode('ascii', 'ignore')
+                if part!='' and part!='-':
+                        summary.append(part)
+        subject = subject[0]
+        story = " ".join(story)
+        summary = " ".join(summary)
+        return [subject,summary,story]
 
 #takes website URL and scrapes fandom subject, summary and story from html to ascii
 def scrape(website):
 
-	if webCheck(website)==False:
-		print "website does not exist"
-		return False
-	return urlRead(website)
+        if webCheck(website)==False:
+                print "website does not exist"
+                return False
+        return urlRead(website)
 
 #takes website URL and returns text array of keywork, summary and story
 
-print scrape('http://archiveofourown.org/works/1158492')
+print scrape('http://archiveofourown.org/works/1113812')
