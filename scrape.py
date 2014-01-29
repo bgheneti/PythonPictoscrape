26084435bf32328c4e189b88a0856e5e60562a2c
bgheneti <bgheneti@mit.edu>
2014-01-28 22:54:28 -0500

    next

--- scrape.py
+++ scrape.py
@@ -12,10 +12,10 @@ import socket
 #        'v=1.0&q=barack%20obama&userip=INSERT-USER-IP')
 
 # 	request = urllib2.Request(url, None, {'Referer': /* Enter the URL of your site here */})
-# 	response = urllib2.urlopen(request)
 
 
-def query(array):
+
+def queryFind(array):
 	query=""
 	for i in xrange(len(array)-1):
 		query+=array[i]+"%20"
@@ -23,12 +23,33 @@ def query(array):
 	query+=array[len(array)-1]
 	return query
 
-
-print query(["a","b","c","d"])
-
-
-def googlePrep(array):
+def urlFind(array,imageRange):
 	
-	query=query(array)
+	query=queryFind(array)
 	ip=socket.gethostbyname(socket.gethostname())
+	queryURLS=[]
+	for x in xrange(imageRange[0],imageRange[1],imageRange[2]):
+		imageNum=str(x)
+		queryURLS.append(('https://ajax.googleapis.com/ajax/services/search/images?' + 'v=1.0&q='+query+'&userip='+ip+'&start='+imageNum))
+	return queryURLS
 
+	#finding the url to search by query = key1 + %20 + key2 ... and image Number
+
+def googlePrep(array,imageRange):
+	
+	queryURLS=urlFind(array,imageRange)
+	for queryURL in queryURLS:
+		request = urllib2.Request(queryURL, None, {'Referer': 'http://www.google.com'})
+		response = urllib2.urlopen(request)
+		results = simplejson.load(response)
+		for result in results['responseData']['results']:
+			    print result['unescapedUrl']
+
+
+print urlFind(["a","bb"],(1,3,1));
+print "****************"
+
+array=["apples","bananas"]
+
+
+googlePrep(array,(1,3,1))
